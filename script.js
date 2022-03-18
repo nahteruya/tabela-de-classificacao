16e5ffaeacc0052c42a8eed1ecfc5d093d934759
nahteruya <nahteruya@gmail.com>
2022-03-18 08:31:16 -0300

    Update files

---
+++ script.js
@@ -0,0 +1,19 @@
+var jogador1 = {
+    nome: "Naomi",
+    vitórias: 2,
+    derrotas: 1,
+    empates: 1,
+    pontos: 7
+};
+
+var jogador2 = {
+    nome: "Kaory",
+    vitórias: 1,
+    derrotas: 2,
+    empates: 1,
+    pontos: 3
+};
+
+var listaJogadores = [jogador1, jogador2];
+
+console.log(jogador1.nome);
