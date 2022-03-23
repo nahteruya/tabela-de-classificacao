4da24a89c3bad1506ef062eafdf45b7f1b0f6141
nahteruya <nahteruya@gmail.com>
2022-03-23 14:27:18 -0300

    Update files

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # tabela-de-classificacao
-Tabela de classificação que faz o cáluco automático dos pontos dos jogadores por meio da quantidade de vitórias, derrotas e empates de cada jogador. 
-O cálculo dos pontos será da seguinte forma:
-- 1 vitória => ganha 3 pontos
-- 1 derrota => perde 1 ponto
-- 1 empate => ganha 2 pontos
+Tabela para gerenciamento de pontos de um jogo 1 vs 1, no qual é possível:
+- adicionar o nome dos jogadores;
+- selecionar, dentre os jogadores adicionados, os 2 jogadores da rodada;
+- classificar o vencedor da rodada ou se houve empate;
+- calcular automaticamente os pontos de cada jogador;
+    O cálculo dos pontos será da seguinte forma:
+    - 1 vitória => ganha 3 pontos
+    - 1 derrota => perde 1 ponto
+    - 1 empate => ganha 2 pontos
+- Remover jogador da tabela de classificação;
+- Zerar a pontuação da tabela.
--- script.js
+++ script.js
@@ -13,10 +13,18 @@ function adicionarJogador() {
     novoJogador.nome = nomeNovoJogador;
     exibirJogadores(listaJogadores);
     document.getElementById("nomeJogador").value = "";
+
+    var elementoDatalist = document.getElementById("jogadores");
+    var elementoOption = document.createElement("option");
+    elementoOption.setAttribute("value", novoJogador.nome);
+    elementoDatalist.appendChild(elementoOption);
 }
 
 function calcularPontos(jogador) {
     var pontos = (jogador.vitorias * 3) + (jogador.empates * 2) - (jogador.derrotas *1);
+    if (pontos < 0){
+        pontos = 0;
+    }
     return pontos;
 }
 
@@ -29,9 +37,6 @@ function exibirJogadores(listaJogadores) {
         elemento += "<td>" + listaJogadores[i].derrotas + "</td>";
         elemento += "<td>" + listaJogadores[i].empates + "</td>";
         elemento += "<td>" + listaJogadores[i].pontos + "</td>";
-        elemento += "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
-        elemento += "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>";
-        elemento += "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>";
         elemento += "<td><button onClick='removerJogador(" + i + ")'>Remover</button></td></tr>";
     }
     var elemementoTabela = document.getElementById("bodyTable");
@@ -40,32 +45,75 @@ function exibirJogadores(listaJogadores) {
 
 exibirJogadores(listaJogadores);
 
-function adicionarVitoria(i) {
-    var jogador = listaJogadores[i];
-    jogador.vitorias++;
-    jogador.pontos = calcularPontos(jogador);
-    exibirJogadores(listaJogadores);
+function adicionarVitoriaJogador1() {
+    var jogador1 = document.getElementById("jogador1").value;
+    var jogador2 = document.getElementById("jogador2").value;
+    for (var i=0; i<listaJogadores.length; i++)
+    {
+        if (jogador1 == listaJogadores[i].nome)
+        {
+            listaJogadores[i].vitorias++;
+        }
+        else if (jogador2 == listaJogadores[i].nome)
+        {
+            listaJogadores[i].derrotas++;
+        }
+        listaJogadores[i].pontos = calcularPontos(listaJogadores[i]);
+        exibirJogadores(listaJogadores);
+    }
+    document.getElementById("jogador1").value = "";
+    document.getElementById("jogador2").value = "";
 }
 
-function adicionarEmpate(i) {
-    var jogador = listaJogadores[i];
-    jogador.empates++;
-    jogador.pontos = calcularPontos(jogador);
-    exibirJogadores(listaJogadores);
+function adicionarVitoriaJogador2() {
+    var jogador1 = document.getElementById("jogador1").value;
+    var jogador2 = document.getElementById("jogador2").value;
+    for (var i=0; i<listaJogadores.length; i++)
+    {
+        if (jogador2 == listaJogadores[i].nome)
+        {
+            listaJogadores[i].vitorias++;
+        }
+        else if (jogador1 == listaJogadores[i].nome)
+        {
+            listaJogadores[i].derrotas++;
+        }
+        listaJogadores[i].pontos = calcularPontos(listaJogadores[i]);
+        exibirJogadores(listaJogadores);
+    }
+    document.getElementById("jogador1").value = "";
+    document.getElementById("jogador2").value = "";
 }
 
-function adicionarDerrota(i) {
-    var jogador = listaJogadores[i];
-    jogador.derrotas++;
-    jogador.pontos = calcularPontos(jogador);
-    exibirJogadores(listaJogadores);
+function adicionarEmpate() {
+    var jogador1 = document.getElementById("jogador1").value;
+    var jogador2 = document.getElementById("jogador2").value;
+    for (var i=0; i<listaJogadores.length; i++)
+    {
+        if (jogador1 == listaJogadores[i].nome || jogador2 == listaJogadores[i].nome)
+        {
+            listaJogadores[i].empates++;
+        }
+        listaJogadores[i].pontos = calcularPontos(listaJogadores[i]);
+        exibirJogadores(listaJogadores);
+    }
+    document.getElementById("jogador1").value = "";
+    document.getElementById("jogador2").value = "";
 }
 
 function removerJogador(i) {
     listaJogadores.splice(i, 1);
-    /* console.log(listaJogadores); */
     exibirJogadores(listaJogadores);
 }
 
-
-
+function zerarPontuacao()
+{
+    for (var i=0; i<listaJogadores.length; i++)
+    {
+        listaJogadores[i].vitorias = 0;
+        listaJogadores[i].derrotas = 0;
+        listaJogadores[i].empates = 0;
+        listaJogadores[i].pontos = calcularPontos(listaJogadores[i]);
+        exibirJogadores(listaJogadores);
+    }
+}
